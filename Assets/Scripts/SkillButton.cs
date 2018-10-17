3c5b1c5825b37f31915a94fdbaad3bb6a4c6f2f9
Ji Hoon Kim <aegookja@naver.com>
2018-10-17 17:16:31 +0200

    Add SkillButton component
    -Begin to implement SkillButton component

---
+++ Assets/Scripts/SkillButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour
+{
+	public Image foreGround;
+
+	public Skill skill;
+
+	private void Start()
+	{
+		if (foreGround.type != Image.Type.Filled)
+		{
+			Debug.LogError("Foreground is not Filled: " + name);
+		}
+	}
+
+	private void Update()
+	{
+		if (skill != null)
+		{
+
+		}
+	}
+}
