60133a1723ae2a026d8f2565a4b31effff43dc9f
EdgeWalker <aegookja@naver.com>
2018-10-21 12:54:06 +0200

    Skill cooldown timer
    -Finished implementation of skill cooldown timer
    -Changed cooldown timer fill direction

--- Assets/Scripts/UI/SkillButton.cs
+++ Assets/Scripts/UI/SkillButton.cs
@@ -8,6 +8,9 @@ public class SkillButton : MonoBehaviour, ISkillMessageTarget
 	public int skillIndex;
 	public Image foreGround;
 
+	private float skillCastTime = 0f;
+	private float cooldownEndTime = 0f;
+
 	private void Start()
 	{
 		if (foreGround.type != Image.Type.Filled)
@@ -17,8 +20,26 @@ public class SkillButton : MonoBehaviour, ISkillMessageTarget
 		}
 	}
 
+	private void Update()
+	{
+		if (Time.time >= cooldownEndTime)
+		{
+			foreGround.gameObject.SetActive(false);
+		}
+		else
+		{
+			foreGround.gameObject.SetActive(true);
+
+			float remainingTime = cooldownEndTime - Time.time;
+			float totalDuration = cooldownEndTime - skillCastTime;
+			foreGround.fillAmount = remainingTime / totalDuration;
+		}
+	}
+
 	public void Cast(float cooldownEndTime)
 	{
 		Debug.Log("I see that skill is Cast!: " + name);
+		this.skillCastTime = Time.time;
+		this.cooldownEndTime = cooldownEndTime;
 	}
 }
